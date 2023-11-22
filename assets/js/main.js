e94767003db3bba99420c800a1859a4259cadf31
billyprime <github@billyprime.com>
2023-11-21 20:11:54 -0500

    Fix image references. Replace JavaScript.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('here');
+  let toggles = document.getElementsByClassName('toggle');
 
+  [...toggles].forEach(toggle => {
+    console.log(toggle.addEventListener);
+    toggle.addEventListener('click', e => {
+      e.target.classList.toggle('hide');
+      document.getElementById(e.target.dataset.ref).classList.toggle('hide');
+    });
+  });
 
-});
+}, false);
